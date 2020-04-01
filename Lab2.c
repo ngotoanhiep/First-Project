b32c986e535e0ff35fed4def698d680810667981
ngotoanhiep <hiepntgcs18405@fpt.edu.vn>
2020-04-01 12:22:48 +0700

    Fix Lab 2 Exercise

--- Lab2.c
+++ Lab2.c
@@ -6,16 +6,16 @@ int main(int argc, char ** argv) {
     int i = 1;
     int S = 0;
     int n = atoi(argv[S]);
+    printf("\n./main.out ");
+    scanf("%d", &n);
     //n = atoi(argv);
-    if (argc <= 2){
-        printf("\n./main.out ");
-        scanf("%d", &n);
+    if (argc > 2){
+        printf("There are so many arguments!\n");
+    }else{
         for(i = 0; i <= n; i++) {
             S = S + i;
         }
         printf("S = %d\n", S);
-    }else{
-        printf("There are so many arguments!\n");
     }
         /*printf("\n./main.out ");
         for(i = 1; i <= n; i++){
